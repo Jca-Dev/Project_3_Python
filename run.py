17fe7f70e463133beb0358a8dbe475143b976987
John <John.Arscott@hotmail.com>
2022-06-10 19:06:01 +0000

    Add function to insert user input to google sheet

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Car-manufacturer-survey')
 
+
 def get_survey_data():
     """
     Get survey results input from the user.
@@ -30,4 +31,19 @@ def get_survey_data():
 
         print(survey_data)
         break
-get_survey_data()
+    return survey_data
+
+
+def update_scoreboard(data, scoreboard):
+    """
+    Prints user input to scoreboard.
+    """
+    print(f"Updating {scoreboard}...\n")
+    scoreboard_to_update = SHEET.worksheet(scoreboard)
+    scoreboard_to_update.append_row(data)
+    print(f"{scoreboard} updated successfully\n")
+
+
+data = get_survey_data()
+survey_data = [int(num) for num in data]
+update_scoreboard(survey_data, "scoreboard")
