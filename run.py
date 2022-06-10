ea779434a8684c437a9d4fcc33eff2a9e6ffb62f
John <John.Arscott@hotmail.com>
2022-06-10 20:17:54 +0000

    add validation function for user input data

--- run.py
+++ run.py
@@ -29,8 +29,10 @@ def get_survey_data():
 
         survey_data = data_in.split(",")
 
-        print(survey_data)
-        break
+        if validate_data(survey_data):
+            print("Data Accepted")
+            break
+
     return survey_data
 
 
@@ -42,6 +44,24 @@ def update_scoreboard(data, scoreboard):
     scoreboard_to_update = SHEET.worksheet(scoreboard)
     scoreboard_to_update.append_row(data)
     print(f"{scoreboard} updated successfully\n")
+
+
+def validate_data(values):
+    """
+    Validate user input data to make sure its within
+    set paramaters in order to align with google sheet.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 10:
+            raise ValueError(
+                f"10 numbers required, You used {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Numbers only: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 data = get_survey_data()
